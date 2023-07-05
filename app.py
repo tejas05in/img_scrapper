64b7a8c979c44e83a4de152b6c544f101f9ad964
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-07-05 11:37:48 +0530

    mongo client changed google chrome headers changed

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import logging
 import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 import os
+from tj_mongo import MongoUrl
 
 app = Flask(__name__)
 
@@ -32,7 +33,7 @@ def index():
 
 
                             # fake user agent to avoid getting blocked by Google
-                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+                    headers = {"User-Agent": "Chrome/114.0.5735.198 Safari/537.36"}
 
                             # fetch the search results page
                     response = requests.get(f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M")
@@ -58,7 +59,7 @@ def index():
                                 img_data.append(mydict)
                                 with open(os.path.join(save_directory, f"{query}_{image_tags.index(image_tag)}.jpg"), "wb") as f:
                                     f.write(image_data)
-                    client = pymongo.MongoClient("mongodb+srv://tejas05in:sajetj@cluster0.cnbjrys.mongodb.net/?retryWrites=true&w=majority")
+                    client = pymongo.MongoClient(MongoUrl.client_url)
                     db = client['image_scrap']
                     review_col = db['image_scrap_data']
                     review_col.insert_many(img_data)          
@@ -74,4 +75,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
